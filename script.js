73a428e8bc9599c3f4183c70df56c39806d70185
Camila <85712574004@senacrs.edu.br>
2025-04-24 12:13:27 -0300

    alteração

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const ovos = [
     {
+        nome: "Ovo do Snoopy com Charlie Brown",
+        preco:  100,
+        imagem: "https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw6e90bb6c/large/1003620_1.png",
+    },
+    {
         nome: "Ovo La Nut",
-        preco: 60,
+        preco:  60,
         imagem: "https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw7ea17042/large/1003507_1.png",
     },
     {
@@ -22,9 +27,26 @@ ovos.forEach(ovos =>{
     
     novaLista.innerHTML =`
         <img class="img1" src="${ovos.imagem}" alt="">
-                <p class="tex1">${ovos.nome}</p>
-                <p class="preco">${ovos.preco.toFixed(2)}</p>
-                <button class="botao">Comprar</button>`;
+            <p class="tex1">${ovos.nome}</p>
+            <div class="container-preco">
+                <p class="preco">R$ ${ovos.preco.toFixed(2)}</p>
+                <button class="botao">Comprar</button>
+            </div> `;
+            listagem.appendChild(novaLista);
+});
 
-                listagem.appendChild(novaLista);
-})
+const btnAbrirCarrinho = document.getElementById("btnAbrirCarrinho");
+const carrinho = document.getElementById("carrinho");
+const btnFecharCarrinho = document.getElementById("btnFecharCarrinho")
+
+function AbrirCarrinho() {
+    carrinho.style.right = "0"
+    btnAbrirCarrinho.style.right = "450px"
+}
+function FecharCarrinho() {
+    carrinho.style.right = "-450px"
+    btnAbrirCarrinho.style.right = "0"
+}
+
+btnAbrirCarrinho.addEventListener('click', AbrirCarrinho)
+btnFecharCarrinho.addEventListener('click', FecharCarrinho)
